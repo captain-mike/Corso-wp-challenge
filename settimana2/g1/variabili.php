3a795086b557efbaeef25f7184eb7e52aeaf8063
captain-mike <info@grafikweb.it>
2022-12-06 13:00:30 +0100

    variabili

---
+++ settimana2/g1/variabili.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+commento su
+ più righe
+*/
+
+//commento ad una sola riga
+
+echo "Hello World!";
+//print("Hello World!"); un po' legacy
+
+/* una variabile è una porzione di memoria in cui salviamo dati 
+Sono indicate con il simbolo del dollaro "$" seguito da un nome
+Il nome deve iniziare con una lettera o con il simbolo _ ($_variabile)
+*/
+$variabile = 5;
+
+echo $variabile;
+
+
+
