39db84f32334735c5d519f01565db5907abfad6e
captain-mike <info@grafikweb.it>
2023-01-11 12:48:08 +0100

    memory

--- settimana4/g5/memory/script.js
+++ settimana4/g5/memory/script.js
@@ -3,19 +3,31 @@
 
 let iconeArray = ['🥥','🍏','🥨','🥦','🍤','🥠','🍖','🍗','🥝','🍻','🥂','🥟'];
 iconeArray = iconeArray.concat(iconeArray);
+let clicks = 0;
+let time = document.querySelector('#time span');
 
 const memory = document.querySelector('#memory');
 
 function start(){
+    // nel caso si stia ricominciando la partita
+    memory.innerHTML = '';//svuoto il memory
+    clicks = 0;//reimposto a zero i click
+    document.getElementById('clicks').innerHTML = 0;//reimposto a zero il contatore
 
+    startTime();//avvio il timer
+
+    //mescolo le icone
     let iconeShuffle = shuffle(iconeArray);
-    for(let icona of iconeShuffle) {
+    //genero le icone
+    iconeShuffle.forEach((icona, i) => {
         
         let box = document.createElement('div');
         box.classList.add('icon-box');
+        box.id = 'box-'+i;
 
-        box.addEventListener('click',() => {
-            checkIcons(icona, box)
+        box.addEventListener('click',function action(){
+            increaseCount();//aumento il contatore
+            checkIcons(box, icona)//avvio il funzionamento
         })
 
         let div = document.createElement('div');
@@ -24,19 +36,86 @@ function start(){
         
         box.append(div);
         memory.append(box);
+    })
+
+}
+
+function startTime(){
+    
+    setInterval(()=>{
+        let seconds = time.textContent || 0;
+        seconds++;
+        time.textContent = seconds;
+    },1000);
+
+}
+
+
+let selectedIcons = [];
+let found = 0;
+function checkIcons(box, icon){
+   
+    box.classList.add('selected');
+    selectedIcons.push({
+        icon:icon,
+        id:box.id
+    });
+
+    if(selectedIcons.length == 2){
+
+        let elem1 = document.querySelector('#'+selectedIcons[0].id)
+        let elem2 = document.querySelector('#'+selectedIcons[1].id)
+
+        //confronta
+        if(selectedIcons[0].icon == selectedIcons[1].icon && elem1.id != elem2.id){
+            elem1.classList.add('found');
+            elem2.classList.add('found');
+            elem1.classList.remove('selected')
+            elem2.classList.remove('selected')
+            found += 2;
+            win();
+        }else{
+            setTimeout(()=>{
+                elem1.classList.remove('selected')
+                elem2.classList.remove('selected')
+            },1500)
+        }
+        //svuota l'array di confro
+        selectedIcons = [];
     }
 
 }
 
-start();
+function increaseCount(){
+    clicks++;
+    document.getElementById('clicks').innerHTML = clicks;
+}
+
+function win(){
 
 
-function checkIcons(icon, box){
-    box.classList.add('selected')
+    if(found == iconeArray.length){
+        Swal.fire({
+            title: 'Hai vinto!',
+            text:`Complimenti, hai vinto con ${clicks} click, e ci hai messo ${time.textContent} secondi`,
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: 'Ricomincia',
+            denyButtonText: `Annulla`,
+        })
+        
+        .then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+              start();
+            }
+        })
+    }
 }
 
 function shuffle(array){
 
+    array = Array.from(array);//faccio una sopia dell'array per non intaccare quello originale
     let newArray = [];
     
     for(let i = array.length; i > 0; i--){
@@ -48,5 +127,9 @@ function shuffle(array){
     }
     return newArray;
 }
+
+
+document.querySelector('#start')
+.addEventListener('click',start)
 
 
